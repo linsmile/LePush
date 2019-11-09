283a292823e964515b15bea80db44100ef3d2e93
AlexZhao <helloalex2020@gmail.com>
2019-11-09 18:53:09 +0700

    fix

--- examples/push_example.php
+++ examples/push_example.php
@@ -3,23 +3,7 @@
 
 require 'config.php';
 
-// 简单推送示例
-// 这只是使用样例,不应该直接用于实际生产环境中 !!
 
-$push_payload = $client->push()
-    ->setPlatform('all')
-    ->addAllAudience()
-    ->setNotificationAlert('Hi, JPush');
-try {
-    $response = $push_payload->send();
-    print_r($response);
-} catch (\JPush\Exceptions\APIConnectionException $e) {
-    // try something here
-    print $e;
-} catch (\JPush\Exceptions\APIRequestException $e) {
-    // try something here
-    print $e;
-}
 
 // 完整的推送示例
 // 这只是使用样例,不应该直接用于实际生产环境中 !!
