3f4a98e4ef9b279a19b0a2e3f355f8edab08646e
AlexZhao <helloalex2020@gmail.com>
2019-11-18 10:33:58 +0700

    example

--- examples/config.php
+++ examples/config.php
@@ -6,5 +6,7 @@ use LePush\LePush as LePush;
 $app_key = getenv('app_key');
 $master_secret = getenv('master_secret');
 $registration_id = getenv('registration_id');
-
-$client = new LePush($app_key, $master_secret);
+$options  =   [
+    'logFile'  => '', //错误日志文件路径
+];
+$client = new LePush($app_key, $master_secret, $options);
